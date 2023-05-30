7883e472e7027b9e93c0f020c48041b549f2c129
Max <maxwangko1@gmail.com>
2023-05-30 15:26:47 -0500

    Init Commit

---
+++ MoodLogger/MoodLogger/MoodLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodLoggerApp.swift
+//  MoodLogger
+//
+//  Created by Max Ko on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
